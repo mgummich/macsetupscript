ad8ef08177f7343497fa451c2efac8344acfea7e
Moreno Gummich <morenogummich@Morenos-MacBook-Pro-2.local>
2021-10-01 13:38:21 +0200

    adding general installation script

---
+++ setup-mac.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Hide "last login" line when starting a new terminal session
+echo 'Hide last Login'
+echo '-----------------'
+touch $HOME/.hushlogin
+
+# Check for Homebrew,
+# Install if we don't have it
+echo 'Checking if Hombrew is installed and if not installing it'
+echo '-----------------'
+if test ! $(which brew); then
+  echo "Installing homebrew..."1
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Run Doctor
+brew doctor
+
+# Update homebrew recipes
+brew update
+
+# Upgrade any already installed formulae
+brew upgrade --all
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+## NVM / NPM Config
+mkdir $HOME/.nvm
+echo 'export NVM_DIR=$HOME/.nvm' >>! $HOME/.zshrc
+echo '[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"' >>! $HOME/.zshrc  # This loads nvm
+echo '[ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && . "$(brew --prefix nvm)/etc/bash_completion.d/nvm"' >>! $HOME/.zshrc  # This loads nvm bash_completion
+
+## Pyen config
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+
+## ZSH config
+# Install powerlevel10k
+echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+#Install autosuggestions
+echo 'source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >>! ~/.zshrc
+
+source .macos
